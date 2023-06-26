3909d09f7f0605f3d0fcfd02a7be026c359b68fa
Nguyễn Trọng Kha <32507584+AhkGnortNeyugn@users.noreply.github.com>
2023-06-26 11:13:40 +0700

    Update duration

--- github.js
+++ github.js
@@ -3,7 +3,7 @@ import { sleep } from 'k6';
 
 export const options = {
     vus: 10,
-    duration: '1m',
+    duration: '2m',
     thresholds: {
         http_req_failed: ['rate<0.01'],
         http_req_duration: ['p(95)<500'],
